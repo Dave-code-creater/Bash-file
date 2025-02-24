acaa607bebeb6b988c4679ee536f305496830e98
Dat Ta <datta@macmini.homeserver.local>
2025-02-24 16:14:55 -0500

    Finish develop, Test requried

--- Linux/RHEL-CentOS/machine-learning.sh
+++ Linux/RHEL-CentOS/machine-learning.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
 
-# Install python, pip, venv, jupyter, numpy, pandas, matplotlib, seaborn, scikit-learn, tensorflow, keras
-sudo dnf install python3 -y
-sudo dnf install python3-pip -y
-sudo dnf install python3-venv -y
-sudo dnf install jupyter -y
-sudo dnf install numpy -y
-sudo dnf install pandas -y
-sudo dnf install matplotlib -y
-sudo dnf install seaborn -y
-sudo dnf install scikit-learn -y
-sudo dnf install tensorflow -y
-sudo dnf install keras -y
-
-# Install R
-sudo dnf install R -y
-
-# Install RStudio
-cd ~/Downloads
-wget https://download1.rstudio.org/electron/rhel9/x86_64/rstudio-2024.12.1-563-x86_64.rpm
-
-sudo dnf install ./rstudio-1.4.1717-x86_64.rpm -y
-
-# Install Jupyter Notebook
-sudo dnf install jupyter-notebook -y
-echo "jupyter notebook" >> ~/.bashrc
+# Note: At the point of writing this script, the tensorflow package is not 
+# officially supports in the RHEL/CentOS.Therfore, i recommend to use Ubuntu or Windows if you want to do machine learning.
